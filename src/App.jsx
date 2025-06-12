531babb9360f8ae30f4ecd2a3d15f9666d2a2794
Amin Amrane <amrane.amin.pro@gmail.com>
2025-06-12 13:42:31 +0200

    Initial commit: Hello World centered

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '2rem'
+    }}>
+      Hello World 🎉
+    </div>
+  );
+}
+
+export default App;
